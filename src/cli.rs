13ef4acb34ce7a894663e3666b2fbf2aef771ea7
Jaden <100730821+JadenLabs@users.noreply.github.com>
2025-06-28 12:22:05 -0600

    feat: improve help message styling and formatting

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,19 @@
 use crate::commands::*;
-use clap::{Parser, Subcommand};
+use anstyle::{AnsiColor, Color, Style};
+use clap::{Parser, Subcommand, builder};
+use colored::Colorize;
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(
+    version,
+    about,
+    long_about = format!(
+        "{} - A project management CLI for quick project manipulation.\n  Run {} to get started.",
+        "RPJ".bold().blue(),
+        "rpj new <name> <directory>".bold().blue()
+    ),
+    styles = get_styles()
+)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
@@ -10,53 +21,112 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
-    #[command(aliases = &["i"], about = "Install a project to the system bins")]
-    Install(InstallCommand),
-
-    #[command(aliases = &["c", "create", "init", "make"], about = "Create a new project")]
-    New(NewCommand),
-
-    #[command(aliases = &["d", "delete", "rm"], about = "Delete an existing project")]
-    Remove(RemoveCommand),
-
-    #[command(aliases = &["u"], about = "Update an existing project")]
-    Update(UpdateCommand),
-
-    #[command(aliases = &["a", "import"], about = "Add an existing project")]
+    #[command(
+        aliases = &["a", "import"],
+        about = format!("Add an existing project using a .rpj file  {}", "(a, import)".dimmed())
+    )]
     Add(AddCommand),
 
-    #[command(aliases = &["e"], about = "Export a project to a file")]
+    #[command(
+        aliases = &["c", "create", "init", "make"],
+        about = format!("Create a new project  {}", "(c, create, init, make)".dimmed())
+    )]
+    New(NewCommand),
+
+    #[command(
+        aliases = &["e"],
+        about = format!("Export a project to a file  {}", "(e)".dimmed())
+    )]
     Export(ExportCommand),
 
-    #[command(aliases = &["l", "ls", "all"], about = "List all projects")]
+    #[command(
+        aliases = &["l", "ls", "all"],
+        about = format!("List all projects  {}", "(l, ls, all)".dimmed())
+    )]
     List(ListCommand),
 
-    #[command(aliases = &["g", "search", "find", "info"], about = "Get information about a project")]
+    #[command(
+        aliases = &["i"],
+        about = format!("Install a project to the system bins  {}", "(i)".dimmed())
+    )]
+    Install(InstallCommand),
+
+    #[command(
+        aliases = &["g", "search", "find", "info"],
+        about = format!("Get information about a project  {}", "(g, search, find, info)".dimmed())
+    )]
     Get(GetCommand),
 
-    #[command(aliases = &["vsc", "edit"], about = "Open project in VS Code")]
+    #[command(
+        aliases = &["vsc", "edit"],
+        about = format!("Open project in VS Code  {}", "(vsc, edit)".dimmed())
+    )]
     Code(CodeCommand),
 
-    #[command(aliases = &["r", "execute", "exec", "start"], about = "Run a project")]
+    #[command(
+        aliases = &["r", "execute", "exec", "start"],
+        about = format!("Run a project  {}", "(r, execute, exec, start)".dimmed())
+    )]
     Run(RunCommand),
 
-    #[command(aliases = &["open-dir", "file", "explorer", "open"], about = "Open project in file explorer")]
+    #[command(
+        aliases = &["u"],
+        about = format!("Update an existing project  {}", "(u)".dimmed())
+    )]
+    Update(UpdateCommand),
+
+    #[command(
+        aliases = &["d", "delete", "rm"],
+        about = format!("Delete an existing project  {}", "(d, delete, rm)".dimmed())
+    )]
+    Remove(RemoveCommand),
+
+    #[command(
+        aliases = &["open-dir", "file", "explorer", "open"],
+        about = format!("Open project in file explorer  {}", "(open-dir, file, explorer, open)".dimmed())
+    )]
     Explore(ExploreCommand),
 
+    #[command(about = "Debug internal state")]
     Debug(DebugCommand),
-
+    // Future:
     // #[command(about = "Get the path of a project")]
     // Path(PathCommand),
-
     // #[command(aliases = &["ter", "shell", "term", "sh", "cmd"], about = "Open a project in the terminal")]
     // Terminal(TerminalCommand),
-
     // #[command(aliases = &["structure"], about = "Show the project structure")]
     // Tree(TreeCommand),
-
     // #[command(aliases = &["label"], about = "Manage tags for a project")]
     // Tag(TagCommand),
-
     // #[command(aliases = &["n", "comment"], about = "Add a note to a project")]
     // Note(NoteCommand),
+}
+
+pub fn get_styles() -> builder::Styles {
+    builder::Styles::styled()
+        .usage(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Green))),
+        )
+        .header(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Green))),
+        )
+        .error(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Red))),
+        )
+        .placeholder(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::Blue))),
+        )
+        .literal(
+            Style::new()
+                .bold()
+                .fg_color(Some(Color::Ansi(AnsiColor::BrightCyan))),
+        )
 }
