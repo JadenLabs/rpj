e2ce602b4438c37546e5ad10471cb2dd3f2e929f
JadenLabs <jadenlabssocial@outlook.com>
2025-07-19 11:09:48 -0600

    feat: add terminal command

--- src/commands/terminal.rs
+++ src/commands/terminal.rs
@@ -1,12 +1,29 @@
+use crate::utils::{get_project_by_name, get_store_path, run_terminal};
+use colored::Colorize;
 
 #[derive(clap::Args)]
 pub struct TerminalCommand {
     pub name: String,
+    #[clap(long, short = 't', help = "The terminal to run the command in")]
+    pub terminal: Option<String>,
 }
 
 impl TerminalCommand {
     pub fn handle(self) -> Result<(), Box<dyn std::error::Error>> {
-       
+        let store_path = get_store_path()?;
+        let project = get_project_by_name(&store_path, &self.name)?;
+
+        let ter_str = &self.terminal.clone().unwrap_or("default".into());
+        println!(
+            "{} {} {}{}{}\n",
+            "ℹ Running".blue(),
+            &self.name.blue().bold(),
+            "using the ".blue(),
+            ter_str.blue().bold(),
+            " terminal.".blue()
+        );
+
+        run_terminal(&project.directory, self.terminal.as_ref(), None)?;
 
         Ok(())
     }
